c959e9b6dd1209f8bdcb14bf630c73ee159cc634
Greenhunk <tanvir.hasan77@yahoo.com>
2024-10-18 00:39:37 +0300

    created a database using sqlalchemy

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
+class Base(DeclarativeBase):
+  pass
 
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+db.init_app(app)
+
+class Books(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str]
+    rating: Mapped[float]
+
+with app.app_context():
+    db.create_all()
+
+
+
+with app.app_context():
+    new_book = Books(id=2, title="Harry Potter", author="J.K", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
 
 all_books = []
-
-
-
 
 @app.route('/')
 def home():
